9ba4b96dbd359807d53edb2ba180c1302982eb73
increase <increasebruce@gmail.com>
2022-06-21 15:06:57 +0100

    Data

---
+++ I4G JS/output.js
@@ -0,0 +1,6 @@
+
+const firstName = "Increase"
+const height = 157.48
+const country = "Nigeria"
+
+console.log("My name is", firstName, "my height in centimetre is", height, "and i am a citizen of", country)
