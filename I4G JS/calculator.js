c9daef880fb312954d82d0fae8e68765430afb4c
increase <increasebruce@gmail.com>
2022-06-21 14:49:19 +0100

    Calculator

---
+++ I4G JS/calculator.js
@@ -0,0 +1,19 @@
+const number = 50;
+// const firstNumber =10
+
+
+//Addition
+const add = number + 10
+console.log(add)
+
+//Subtraction
+const subtract = number - 20
+console.log(subtract)
+
+//divide
+const divide = number /5
+console.log(divide)
+
+// multiply
+const multiply = number * 10
+console.log(multiply)
